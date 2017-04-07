6dc5f371c082afbe383b2a732ffba1279ea74837
royado@hotmail.com <royado@hotmail.com>
2017-04-06 23:03:13 -0500

    ultimo cambios sistema de inventario

---
+++ angular/appIni.js
@@ -0,0 +1,54 @@
+var App=angular.module('appIni',['ngNewRouter']);
+// App.config(function($stateProvider,$urlServiceProvider){//,$urlRouteerProvider){
+//
+// $locationProvider.hashPrefix("!");
+//   //$urlRouterProvider.when("", "/administrador");
+//   $stateProvider
+//   .state('unidad',{
+//       url:'/undad',
+//       templateUrl:'views/maintenance/frmunidad.html'
+//   })
+//   .state('categoria',{
+//       url:'/categoria',
+//       templateUrl:'./views/maintenance/frmunidad.html'//,
+//   })
+//   .state('articulo',{
+//       url:'/articulo',
+//       templateUrl:'/views/maintenance/frmunidad.html'//,
+//   });
+//   $locationProvider.html5Mode(true);
+// });
+// App.config(function($stateProvider,$routeProvider){
+//   $stateProvider
+//   .state('unidad',{
+//     url:"/unidad",
+//     templateUrl:"/views/maintenance/frmunidad.html"
+//   })
+//   .state('categoria',{
+//       url:"/categoria",
+//     templateUrl:"./views/maintenance/frmunidad.html"
+//   });
+//
+// });
+
+App.controller('adminPrincipal',function($scope,$location,$window,$router){
+
+  $router.config([
+      {
+        path:'unidad',
+        components:{
+          'uni':'./views/maintenance/frmunidad.html'
+        }
+      }
+
+  ])
+});
+// App.controller('controllerunidad',function($scope,$location,$window){
+//
+// });
+// App.controller('controllercategoria',function($scope,$location,$window){
+//
+// });
+// App.controller('controllerarticulo',function($scope,$location,$window){
+//
+// });
